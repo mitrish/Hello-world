8a666aa79b0833d438735dab4de715706679d9e1
Rishabh <rishabhmittal200@gmail.com>
2018-10-08 12:37:12 +0530

    Created objectoriented.py
    A file that teaches how to use object oriented in python. created a hello world class which has two functions that prints hello world and a Github username

---
+++ Python/ObjectOrientedInPython.py
@@ -0,0 +1,12 @@
+class HelloWorld:
+  def PrintHelloWorld():
+    print("Hello World")
+  
+  def PrintGithubName():
+    print("mitrish)
+class main:
+  def main():
+    //create an object of class HelloWorld
+    object = HelloWorld()
+    object.PrintHelloWorld()
+    object.PrintGithubName
